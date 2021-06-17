7324e4a70264eefa8e5d9e83c7b8a0c2e295a420
不动声色 <1609059419@qq.com>
2021-06-18 00:06:02 +0800

    icon组件基础完成

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,3 +1,46 @@
+import React, { CSSProperties } from 'react'
+import classNames from 'classnames'
+import *  as antIcons from '@ant-design/icons'
+
+type ThememProps = 'primary' | 'info' | 'success' | 'processing' | 'error' | 'highlight' | 'warning' | 'normal' | 'white' | 'black';
 
 
+interface IconProps {
+    theme?: ThememProps,
+    className?: string,
+    rotate?: number,
+    spin?: boolean,
+    style?: CSSProperties,
+    twoToneColor?: string,
+    name: keyof typeof antIcons
+};
 
+const Icon: React.FC<IconProps> = ({
+    theme = 'black',
+    className,
+    rotate,
+    spin,
+    style,
+    twoToneColor = 'red',
+    name
+}) => {
+    const AntIcon = antIcons[name] as any;
+    const realClassNames = classNames(
+        {
+            [`rrw-icon-${theme}`]: theme
+        },
+        className
+    );
+
+    return (
+        <AntIcon
+            style={style}
+            rotate={rotate}
+            spin={spin}
+            className={realClassNames}
+            twoToneColor={twoToneColor}
+        />
+    )
+}
+
+export default Icon;
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.less';
 
-import { SmileFilled, SmileTwoTone } from '@ant-design/icons';
-import * as icons from '@ant-design/icons'
-console.log(icons.SmileFilled)
+// import { SmileFilled, SmileTwoTone } from '@ant-design/icons';
+// import * as icons from '@ant-design/icons'
+// console.log(icons.SmileFilled)
+
+import Icon from './components/icon/icon'
 
 ReactDOM.render(
   <React.StrictMode>
-     <SmileFilled 
-      spin
-      twoToneColor="red"
-     />
+    <Icon
+      name='SmileFilled'
+      theme='warning'
+      twoToneColor='green'
+    />
+     <Icon
+      name='SmileFilled'
+      theme='error'
+      twoToneColor='green'
+    />
+     <Icon
+      name='SmileFilled'
+      theme='success'
+      twoToneColor='green'
+    />
+      <Icon
+      name='SmileFilled'
+      theme='success'
+      twoToneColor='green'
+      style={{color:'purple'}}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
