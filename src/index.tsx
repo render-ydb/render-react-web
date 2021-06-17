c6f30d8fdb3d267bf5f27ae75d7129a66b156578
不动声色 <1609059419@qq.com>
2021-06-17 22:22:53 +0800

    集成ant/icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.less';
+import './styles/index.less';
+
+import { SmileFilled, SmileTwoTone } from '@ant-design/icons';
+import * as icons from '@ant-design/icons'
+console.log(icons.SmileFilled)
 
 ReactDOM.render(
   <React.StrictMode>
-    1
+     <SmileFilled 
+      spin
+      twoToneColor="red"
+     />
   </React.StrictMode>,
   document.getElementById('root')
 );
