6d56fa2495d3751d020fdd8aa7defb70a6d95fc3
不动声色 <1609059419@qq.com>
2021-07-24 23:40:59 +0800

    xxx

--- src/components/alert/alert.test.tsx
+++ src/components/alert/alert.test.tsx
@@ -2,9 +2,6 @@ import { render } from '@testing-library/react'
 import Alert from './alert'
 
 test('Alert是否存在文档中', () => {
-    // const wrapper = render(<Icon name='AimOutlined'></Icon>);
-    // const element = wrapper.getByRole('img');
-    // expect(element).toBeInTheDocument();
 })
 
 
