0e1fefb12c35e9cb498dd25f0e88605a5ef0f1d5
不动声色 <1609059419@qq.com>
2021-07-24 22:52:17 +0800

    update

--- src/components/alert/alert.test.tsx
+++ src/components/alert/alert.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react'
 import Alert from './alert'
 
-test('icon是否存在文档中', () => {
+test('Alert是否存在文档中', () => {
     // const wrapper = render(<Icon name='AimOutlined'></Icon>);
     // const element = wrapper.getByRole('img');
     // expect(element).toBeInTheDocument();
