da7d8075bb7690d3c037d8c3e33aaf75853a6a4e
不动声色 <1609059419@qq.com>
2021-07-24 22:19:01 +0800

    test travis

--- src/components/Tag/tag.test.tsx
+++ src/components/Tag/tag.test.tsx
@@ -5,6 +5,7 @@ test('icon是否存在文档中', () => {
     // const wrapper = render(<Icon name='AimOutlined'></Icon>);
     // const element = wrapper.getByRole('img');
     // expect(element).toBeInTheDocument();
+    
 })
 
 
